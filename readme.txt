0b9e81276da0f8bf2e97a29b4e823a22a7a7b998
abiha-inam123 <61555456+abiha-inam123@users.noreply.github.com>
2021-07-01 03:56:22 -0700

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,3 @@
+first edit your configuration of each file to its allocated path
+run the server
+run  the client enjoy minecraft mini
---
+++ run.py
@@ -0,0 +1,83 @@
+import minecraft
+import pygame
+from network import Network
+class Game:
+
+    def __init__(self, w, h):
+        self.net = Network()
+        self.width = w
+        self.height = h
+        self.player = minecraft.Model()
+        self.player2 = minecraft.Model()
+        self.canvas = Canvas(self.width, self.height, "Testing...")
+
+    def run(self):
+        clock = pygame.time.Clock()
+        run = True
+        while run:
+            clock.tick(60)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+                if event.type == pygame.K_ESCAPE:
+                    run = False
+
+            # Send Network Stuff
+            self.player2.x, self.player2.y = self.parse_data(self.send_data())
+
+            # Update Canvas
+            self.canvas.draw_background()
+            self.player.draw(self.canvas.get_canvas())
+            self.player2.draw(self.canvas.get_canvas())
+            self.canvas.update()
+
+        pygame.quit()
+
+    def send_data(self):
+        """
+        Send position to server
+        :return: None
+        """
+        data = str(self.net.id) + ":" + str(self.player.x) + "," + str(self.player.y)
+        reply = self.net.send(data)
+        return reply
+
+    @staticmethod
+    def parse_data(data):
+        try:
+            d = data.split(":")[1].split(",")
+            return int(d[0]), int(d[1])
+        except:
+            return 0,0
+
+
+class Canvas:
+
+    def __init__(self, w, h, name="None"):
+        self.width = w
+        self.height = h
+        self.screen = pygame.display.set_mode((w,h))
+        pygame.display.set_caption(name)
+
+    @staticmethod
+    def update():
+        pygame.display.update()
+
+    def draw_text(self, text, size, x, y):
+        pygame.font.init()
+        font = pygame.font.SysFont("comicsans", size)
+        render = font.render(text, 1, (0,0,0))
+
+        self.screen.draw(render, (x,y))
+
+    def get_canvas(self):
+        return self.screen
+
+    def draw_background(self):
+        self.screen.fill((255,255,255))
+
+if __name__ == "__main__":
+    g = minecraft.main()
+    g.run()
